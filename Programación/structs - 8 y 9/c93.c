ff3f9a0254e1760ff56cd7e4993cdf41435b245b
Fatatouille <108636498+Fatatouille@users.noreply.github.com>
2024-07-22 15:53:59 -0300

    Add files via upload

---
+++ Programación/structs - 8 y 9/c93.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct pedidos{
+    
+}typedef pedidos;
+
+struct arbol{
+    char especie[50], categoria[50], vulgar[50];
+    int stock;
+    float precio;
+    int *pedidos;
+} typedef Arbol;
+
+int main(){
+
+
+    return 0;
+}
